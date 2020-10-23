f7910cc5efa8dfdeea2edf61dafcc1940388b394
Arseny Petrovich <strangesound@gmail.com>
2020-10-23 23:39:53 +0300

    refactor&nested bem

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,33 @@
-let editBtn = document.querySelector('.avatar__edit-icon');
-let clsBtn = document.querySelector('.form-popup__close-btn');
+let editButton = document.querySelector('.avatar__edit-icon');
+let closeButton = document.querySelector('.form-popup__close-btn');
 let popUpForm = document.querySelector('.form-popup');
-let popupBtn = document.querySelector('.form-popup__btn');
-
-function addPopup() {
-    popUpForm.style.display = 'flex'
-    document.getElementById('first-name').value = document.querySelector('.avatar__head').textContent;;
-    document.getElementById('job-name').value = document.querySelector('.avatar__subtitle').textContent;
-};
-
-
-function clsPopup() {
-    popUpForm.style.display = 'none'
-};
-
-
-// Находим форму в DOM
+let popupButton = document.querySelector('.form-popup__btn');
+let formNameInput = document.querySelector('.form-popup__name');
+let formJobInput = document.querySelector('.form-popup__job');
+let pageName = document.querySelector('.avatar__head');
+let pageJob = document.querySelector('.avatar__subtitle');
 let formElement = document.querySelector('.form-popup__form')
 
+
+function addPopup() {
+    popUpForm.classList.add('popup_opened')
+    formNameInput.value = pageName.textContent;;
+    formJobInput.value = pageJob.textContent;
+}
+
+function clsPopup() {
+    popUpForm.classList.remove('popup_opened')
+}
+
 function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let nameInput = document.getElementById('first-name').value;
-    let jobInput = document.getElementById('job-name').value;
-
-    nameAvatar = document.querySelector('.avatar__head');
-    jobAvatar = document.querySelector('.avatar__subtitle');
-
-    // Вставьте новые значения с помощью textContent
-    nameAvatar.textContent = nameInput
-    jobAvatar.textContent = jobInput
+    evt.preventDefault();
+    pageName.textContent = formNameInput.value
+    pageJob.textContent = formJobInput.value
     clsPopup()
 }
 
+
 formElement.addEventListener('submit', formSubmitHandler);
-editBtn.addEventListener('click', addPopup);
-clsBtn.addEventListener('click', clsPopup);
+editButton.addEventListener('click', addPopup);
+closeButton.addEventListener('click', clsPopup);
+// popUpForm.addEventListener('click', clsPopup);
