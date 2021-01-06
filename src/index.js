1a82608dd2a603467efadf05ad21f0244b824c42
Arseny Petrovich <strangesound@gmail.com>
2021-01-06 11:02:13 +0300

    move files to src

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { initialCards } from '../utils/initialCards.js';
-import { Card } from '../components/Card.js';
-import { Section } from '../components/Section.js';
-// import { Popup } from '../components/Popup.js';
-import { PopupWithImage } from '../components/PopupWithImage.js';
-import { PopupWithForm } from '../components/PopupWithForm.js';
-import { FormValidator } from '../components/FormValidator.js'
-import { UserInfo } from '../components/UserInfo.js';
-import '../src/index.css';
+import { initialCards } from './utils/initialCards.js';
+import { Card } from './components/Card.js';
+import { Section } from './components/Section.js';
+// import { Popup } from './components/Popup.js';
+import { PopupWithImage } from './components/PopupWithImage.js';
+import { PopupWithForm } from './components/PopupWithForm.js';
+import { FormValidator } from './components/FormValidator.js'
+import { UserInfo } from './components/UserInfo.js';
+import './index.css';
 
 
 const formEditUserInfoButton = document.querySelector('.avatar__edit-icon');
--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './src/index.html'
     }),
     new MiniCssExtractPlugin(),
 
